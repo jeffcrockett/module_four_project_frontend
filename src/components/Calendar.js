080a46669de28acb8610c4c9a86bf64bce8eb0ed
github username <github email address>
2018-09-27 13:43:41 -0400

    Calendar build first shot, using semantic-ui components

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,110 @@
-import React from 'react'
+import React from "react";
+import { Grid, Image } from "semantic-ui-react";
 
-const Calendar = (props) => {
-    return (
-        <div>Calendar</div>
-    )
-}
+const Calendar = props => {
+  return (
+    <div>
+      Calendar
+      <Grid columns={7} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+        </Grid.Row>
 
-export default Calendar
+        <Grid.Row>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+};
+
+export default Calendar;
+// export default BigCalendar;
