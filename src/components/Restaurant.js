c9a935fa1a9c212ee878f2e2d09490f05eac2699
github username <github email address>
2018-09-28 10:05:51 -0400

    detail view changes made, with date selector and beginnings of btn functions

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,26 +1,31 @@
 import React from "react";
 
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image } from "semantic-ui-react";
+import noun_Food from "../noun_Food.png";
 
-const Restaurant = ({restaurant, selectRestaurant}) => (
+const Restaurant = ({ restaurant, selectRestaurant }) => (
   <Card onClick={() => selectRestaurant(restaurant)}>
-    {!restaurant.restaurant.thumb ?
-    <Icon name="food"/>
-  : <Image src={restaurant.restaurant.thumb} />}
+    {!restaurant.restaurant.thumb ? (
+      <Image src={noun_Food} size="small" centered />
+    ) : (
+      <Image src={restaurant.restaurant.thumb} />
+    )}
     <Card.Content>
       <Card.Header>{restaurant.restaurant.name}</Card.Header>
       <Card.Meta>
-        <span className='date'>{restaurant.restaurant.cuisines}</span>
+        <span className="date">{restaurant.restaurant.cuisines}</span>
       </Card.Meta>
-      <Card.Description>{restaurant.restaurant.location.locality}</Card.Description>
+      <Card.Description>
+        {restaurant.restaurant.location.locality}
+      </Card.Description>
     </Card.Content>
     <Card.Content extra>
       <a>
-        <Icon name='user' />
+        <Icon name="user" />
         22 Friends
       </a>
     </Card.Content>
   </Card>
-)
+);
 
-export default Restaurant
+export default Restaurant;
--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -1,12 +1,80 @@
 import React from "react";
 
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Grid, Button } from "semantic-ui-react";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import "react-datepicker/dist/react-datepicker.css";
 
-const RestaurantDetail = ({ restaurant }) => {
-    return <Card>
-        <h1>{restaurant.restaurant.name}</h1>
-    </Card>
+class RestaurantDetail extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      mealDate: moment()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      mealDate: date
+    });
+  }
+
+  handleAddPick() {
+    const data = {
+      user_id: null,
+      restaurant_id: this.props.restaurant.restaurant.R.res_id,
+      date: this.state.mealDate._d,
+      votes: 1,
+      confirmed: false
+    };
+    fetch("https://localhost:3000/picks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringfify(data)
+    })
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }
+
+  render() {
+    return (
+      <Grid columns={2}>
+        <Grid.Column>
+          <Card>
+            <h1>{this.props.restaurant.restaurant.name}</h1>
+            <div>
+              Address: {this.props.restaurant.restaurant.location.address}
+            </div>
+            <div>Cuisine(s): {this.props.restaurant.cuisines}</div>
+            <div>
+              User Rating:{" "}
+              {this.props.restaurant.restaurant.user_rating.rating_text}
+            </div>
+            <div>
+              Cost for Two: ${
+                this.props.restaurant.restaurant.average_cost_for_two
+              }
+            </div>
+          </Card>
+        </Grid.Column>
+        <Grid.Column>
+          <Grid.Row>A map maybe?</Grid.Row>
+          <Grid.Row>
+            <Button onClick={this.handleAddPick}>Add to your picks</Button>
+            <DatePicker
+              selected={this.state.mealDate}
+              onChange={this.handleChange}
+            />
+            <Button>Btn 2</Button>
+          </Grid.Row>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
-
-export default RestaurantDetail
+export default RestaurantDetail;
